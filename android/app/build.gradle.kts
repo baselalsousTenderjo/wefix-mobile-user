a7299da0efa4c0873cc7001f7c35481465697e16
Jadaboawwad <jadaboawwad@gmail.com>
2025-12-30 17:22:32 +0300

    release : first release in app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
-        versionCode = 12
+        versionCode = 13
         versionName = flutter.versionName
     }
 
