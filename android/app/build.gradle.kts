e850add0e3324305af622169ca93a0071c009caa
Jadaboawwad <jadaboawwad@gmail.com>
2026-01-25 16:14:06 +0300

    deploy : android release 1.0.20

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
     minSdk = 25
         targetSdk = flutter.targetSdkVersion
-        versionCode = 22
+        versionCode = 23
         versionName = flutter.versionName
     }
 
