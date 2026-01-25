dbb068fdc1cc4a30ca197a7e2f44776a73d54263
Jadaboawwad <jadaboawwad@gmail.com>
2026-01-25 16:03:35 +0300

    deploy : android release 1.1.23

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         // Ensure it's at least 23 for firebase_auth
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 37
+        versionCode = 39
         versionName = flutter.versionName
     }
 
